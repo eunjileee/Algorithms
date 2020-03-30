20eb6a0bf11091ad300b0ca656f5f03b288fde68
eunjileee <leeeeunz@gmail.com>
2020-03-30 11:59:49 +0900

    BOJ #1000

---
+++ BOJ/BOJ#1000.js
@@ -0,0 +1,23 @@
+/** 
+- Author: eunjileee@GitHub (leeeeunz@gmail.com)
+- Date : 2020-03-30
+- URL : https://www.acmicpc.net/problem/1000
+*/
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on("line", function(line) {
+  const input = line.split(" ");
+
+  const result = Number(input[0]) + Number(input[1]);
+  console.log(result);
+
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
