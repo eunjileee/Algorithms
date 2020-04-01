3f24f43544743de8f665f330dbf4bc684c052581
eunjileee <leeeeunz@gmail.com>
2020-04-01 14:04:03 +0900

    BOJ #10953
    This reverts commit fe7c621ec35f72872482cb3567fbe7c7699b0761.

--- BOJ/BOJ#10953.js
+++ BOJ/BOJ#10953.js
@@ -1,7 +1,7 @@
 /** 
 - Author: eunjileee@GitHub (leeeeunz@gmail.com)
 - Date : 2020-03-30
-- URL : https://www.acmicpc.net/problem/10952
+- URL : https://www.acmicpc.net/problem/10951
 */
 
 const readline = require("readline");
@@ -17,13 +17,11 @@ rl.on("line", function(line) {
   input.push(line);
   /*
   예시)
-  < 입력 >                         < 입력을 받은 line 배열 >
+  < 입력 >       < 입력을 받은 line 배열 >
     1 1
-    2 3      [ [ '1 1' ], [ '2 3' ], [ '3 4' ] [ '9 8' ] [ '5 2' ] [ '0 0' ] ]
+    2 3      [ [ '1 1' ], [ '2 3' ], [ '3 4' ] [ '4 5'] ]
     3 4
-    9 8
-    5 2
-    0 0
+    4 5
   */
 }).on("close", function() {
   let num = [];
@@ -32,14 +30,12 @@ rl.on("line", function(line) {
   }
 
   /*
-
     <num의 배열>
-    [ ['1', '1'], ['2', '3'], ['3', '4'], ['9', '8'], ['5', '2'], ['0', '0'] ]
-
-  */
+    [ ['1', '1'], ['2', '3'], ['3', '4'], ['4', '5'] ]
+    */
 
   /* 배열 안 요소를 인덱스로 접근하여 합한다.*/
-  for (let i = 0; i < input.length - 1; i++) {
+  for (let i = 0; i < input.length; i++) {
     num1 = Number(num[i][0]);
     num2 = Number(num[i][1]);
 
