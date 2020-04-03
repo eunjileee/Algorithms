a4f843a90dc78d2fa0cb6c68bcd3234caadd0ea8
eunjileee <leeeeunz@gmail.com>
2020-04-03 10:51:14 +0900

    BOJ #11718

---
+++ BOJ/BOJ#11718.js
@@ -0,0 +1,34 @@
+/** 
+- Author: eunjileee@GitHub (leeeeunz@gmail.com)
+- Date : 2020-04-03
+- URL : https://www.acmicpc.net/problem/11718
+*/
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on("line", function(line) {
+  input.push(line);
+
+  /*
+
+  Hello                 <input>
+  Baekjoon              [ 'Hello', ['Baekjoon'], ['Online Judge'] ]
+  Online Judge
+
+  */
+}).on("close", function() {
+  let arr = [];
+
+  for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+  }
+
+  process.exit();
+});
