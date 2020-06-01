5cbf14781c62ef023c2add88d49515b41929ac00
eunjileee <leeeeunz@gmail.com>
2020-06-01 10:16:30 +0900

    LC #1 added solution

--- LeetCode/LC#1.js
+++ LeetCode/LC#1.js
@@ -11,12 +11,26 @@
  * @return {number[]}
  */
 
-var twoSum = function (nums, target) {
+// FIRST SOLUTION
+let twoSum = function (nums, target) {
   for (i = 0; i < nums.length; i++) {
     for (j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
     }
+  }
+};
+
+// SECOND SOLUTION
+let twoSUm = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let value = target - nums[i];
+    if (map.has(value)) {
+      return [map.get(value), i];
+    }
+    map.set(nums[i], i);
   }
 };
