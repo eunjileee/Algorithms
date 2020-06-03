51651f7d86a8560d1dad9fbe21288a0298a4a5cc
eunjileee <leeeeunz@gmail.com>
2020-06-03 23:43:57 +0900

    LC #20

--- LeetCode/LC#20.js
+++ LeetCode/LC#20.js
@@ -5,3 +5,16 @@
 - URL : https://leetcode.com/problems/valid-parentheses/
 */
 
+let isValid = function (s) {
+  if (s === null || s.length <= 0) return true;
+  var cArr = s.split("");
+  var stack = [];
+  for (var c of cArr) {
+    if (c === "[") stack.push("]");
+    else if (c === "{") stack.push("}");
+    else if (c === "(") stack.push(")");
+    else if (stack.length === 0 || c !== stack.pop()) return false;
+  }
+  if (stack.length === 0) return true;
+  return false;
+};
